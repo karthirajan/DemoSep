c5df704467341ce456993c88206465522fc10259
karthirajan <karthick@kripya.com>
2020-09-24 20:40:32 +0530

    2 more new packages created by tester

---
+++ src/org/second/A.java
@@ -0,0 +1,18 @@
+package org.second;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class A {
+	
+	public static void main(String[] args) {
+		
+		List<String> li = new ArrayList<>();
+		li.add("GIT");
+		
+		System.out.println(li);
+		
+		
+	}
+
+}
